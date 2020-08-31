2f3dff4784af8f8922b0cdf307a6ac234a02e99f
matayuuu415@gmail.com <urasoe48>
2020-08-31 22:48:47 +0900

    2

--- lib/Setup/signIn.dart
+++ lib/Setup/signIn.dart
@@ -1,9 +1,9 @@
-//ログイン画面用Widget
+// ログイン画面用Widget
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
 import 'package:mychat2_app/main.dart';
+import 'package:provider/provider.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -11,21 +11,21 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
-  //メッセージ表示用
+  // メッセージ表示用
   String infoText = '';
-  //入力したメール右アドレス・パスワード
+
+  // 入力したメールアドレス・パスワード
   String email = '';
   String password = '';
 
   @override
   Widget build(BuildContext context) {
+    // ユーザー情報を受け取る
     final UserState userState = Provider.of<UserState>(context);
 
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Form(
-        key: _formkey,
+      body: Center(
         child: Container(
           padding: EdgeInsets.all(24),
           child: Column(
@@ -52,71 +52,107 @@ class _LoginPageState extends State<LoginPage> {
                 padding: EdgeInsets.all(40),
               ),
               //メールアドレスの入力
-              TextFormField(
-                validator: (input) {
-                  if (input.isEmpty) {
-                    return 'Please type au email';
-                  }
-                },
-                onSaved: (input) => email = input,
-                decoration: InputDecoration(labelText: 'メールアドレス'),
-              ),
-              //パスワード入力
-              TextFormField(
-                validator: (input) {
-                  if (input.length < 6) {
-                    return 'Your password needs to be atleast 6 chaacters';
-                  }
-                },
-                obscureText: true,
-                onSaved: (input) => password = input,
-                decoration: InputDecoration(labelText: 'パスワード'),
-              ),
 
+              // メールアドレス入力
+              TextFormField(
+                decoration: InputDecoration(labelText: 'メールアドレス'),
+                onChanged: (String value) {
+                  setState(() {
+                    email = value;
+                  });
+                },
+              ),
+              // パスワード入力
+              TextFormField(
+                decoration: InputDecoration(labelText: 'パスワード'),
+                obscureText: true,
+                onChanged: (String value) {
+                  setState(() {
+                    password = value;
+                  });
+                },
+              ),
               Container(
                 padding: EdgeInsets.all(8),
-                //メッセージ表示
+                // メッセージ表示
                 child: Text(infoText),
               ),
               Container(
                 width: double.infinity,
-                //ユーザー登録ボタン
+                // ユーザー登録ボタン
                 child: RaisedButton(
-                    color: Colors.orange,
-                    textColor: Colors.white,
-                    child: Text('ユーザー登録'),
-                    onPressed: () {
-                      signIn();
-                    }),
+                  color: Colors.orange,
+                  textColor: Colors.white,
+                  child: Text('ユーザー登録'),
+                  onPressed: () async {
+                    try {
+                      // メール/パスワードでユーザー登録
+                      final FirebaseAuth auth = FirebaseAuth.instance;
+                      final result = await auth.createUserWithEmailAndPassword(
+                        email: email,
+                        password: password,
+                      );
+                      // ignore: deprecated_member_use
+                      final FirebaseUser user = result.user;
+
+                      // ユーザー情報を更新
+                      userState.setUser(user);
+
+                      // ユーザー登録に成功した場合
+                      // チャット画面に遷移＋ログイン画面を破棄
+                      await Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (context) {
+                          return ChatPage();
+                        }),
+                      );
+                    } catch (e) {
+                      // ユーザー登録に失敗した場合
+                      setState(() {
+                        infoText = "登録に失敗しました：${e.message}";
+                      });
+                    }
+                  },
+                ),
               ),
               Container(
-                  width: double.infinity,
-                  //ログインぽたん
-                  child: OutlineButton(
-                      textColor: Colors.orange,
-                      child: Text('ログイン'),
-                      onPressed: () {}))
+                width: double.infinity,
+                // ログイン登録ボタン
+                child: OutlineButton(
+                  textColor: Colors.orange,
+                  child: Text('ログイン'),
+                  onPressed: () async {
+                    try {
+                      // メール/パスワードでログイン
+                      final FirebaseAuth auth = FirebaseAuth.instance;
+                      final result = await auth.signInWithEmailAndPassword(
+                        email: email,
+                        password: password,
+                      );
+                      final FirebaseUser user = result.user;
+
+                      // ユーザー情報を更新
+                      userState.setUser(user);
+
+                      // ログインに成功した場合
+                      // チャット画面に遷移＋ログイン画面を破棄
+                      await Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (context) {
+                          return ChatPage();
+                        }),
+                      );
+                    } catch (e) {
+                      // ログインに失敗した場合
+                      setState(() {
+                        infoText = "ログインに失敗しました：${e.message}";
+                      });
+                    }
+                  },
+                ),
+              ),
             ],
           ),
         ),
       ),
     );
-  }
-
-  Future<void> signIn() async {
-    final formState = _formkey.currentState;
-    if (formState.validate()) {
-      //firebaseにログイン
-      formState.save();
-      try {
-        FirebaseUser user = (await FirebaseAuth.instance
-                .signInWithEmailAndPassword(email: email, password: password))
-            as FirebaseUser;
-        Navigator.push(
-            context, MaterialPageRoute(builder: (context) => ChatPage()));
-      } catch (e) {
-        print(e.massage);
-      }
-    }
   }
 }
